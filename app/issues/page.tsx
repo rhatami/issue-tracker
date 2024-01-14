f8d218164d95c346d53f4b2720ce21d8af9f9473
RHB <rhb2020@gmail.com>
2024-01-15 00:43:29 +0330

    build new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import { Button } from "@radix-ui/themes";
 
 const issuesPage = () => {
   return (
     <div>
-      <Button>issuesPage</Button>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 };
